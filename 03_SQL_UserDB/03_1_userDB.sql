8e0ce281d5794a7dead9906bb7fc912619ab1340
przygoda <schmidt.multimedia@googlemail.com>
2020-10-28 12:02:36 +0000

    update feld plz

--- 03_SQL_UserDB/03_1_userDB.sql
+++ 03_SQL_UserDB/03_1_userDB.sql
@@ -10,21 +10,27 @@ DROP TABLE IF EXISTS users;
 
 CREATE TABLE users
 (
+    id INT NOT NULL,
     user_name VARCHAR(20) NOT NULL PRIMARY KEY,
     first_name VARCHAR(20) NOT NULL,
     family_name VARCHAR(20) NOT NULL
 );
 
+#DESC users;
+
+INSERT INTO users (id,user_name,first_name,family_name) VALUES(1,"max","Max","Mütze");
+INSERT INTO users (id,user_name,first_name,family_name) VALUES(2,"max01","Max","Martinelli");
+INSERT INTO users (id,user_name,first_name,family_name) VALUES(3,"max29","Max","Martinelli");
+
+ALTER TABLE users ADD plz INT(5) NOT NULL DEFAULT 12345;
+
 DESC users;
-
-INSERT INTO users (user_name,first_name,family_name) VALUES("max","Max","Mütze");
-
 SELECT * FROM users;
 
 /*
 
 INSERT INTO users (user_name,first_name,family_name) VALUES("max","Max","Martinelli");
 
-
+SELECT * FROM users;
 
 */
