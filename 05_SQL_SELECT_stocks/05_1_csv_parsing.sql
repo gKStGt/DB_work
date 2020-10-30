a9fb23ed1c14fc4a8f40307850887d3f3015cf21
przygoda <schmidt.multimedia@googlemail.com>
2020-10-30 10:17:10 +0000

    update 2 stocks

---
+++ 05_SQL_SELECT_stocks/05_1_csv_parsing.sql
@@ -0,0 +1,39 @@
+/* Parsen der csv-Datei / DB + Tab anlegen*/
+
+system clear;
+
+-- DB anlegen, falls diese noch nict existiert
+CREATE DATABASE IF NOT EXISTS stocks;
+#SHOW DATABASES;
+
+-- focus
+USE stocks;
+
+-- TAB löschen, falls existent
+DROP TABLE IF EXISTS ccc_list;
+#SHOW TABLES;
+
+-- TAB anlegen, falls diese noch nicht existiert
+CREATE TABLE IF NOT EXISTS stocks.ccc_list
+(
+    id INT NOT NULL PRIMARY KEY,
+    c_name VARCHAR(100) NOT NULL,
+    ticker VARCHAR(10) NOT NULL,
+    sector VARCHAR(100) NOT NULL,
+    industry VARCHAR(100) NOT NULL,
+    no_years INT NOT NULL,
+    ranking INT NOT NULL,
+    price DECIMAL(6,2) NOT NULL,
+    yield DECIMAL(6,4) NOT NULL,
+    dividend DECIMAL(6,4) NOT NULL,
+    payouts INT NOT NULL,
+    annualized DECIMAL(4,2) NOT NULL
+);
+
+-- TAB Schema anzeigen
+DESC ccc_list;
+
+
+
+
+
