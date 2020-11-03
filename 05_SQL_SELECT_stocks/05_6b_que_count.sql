1e38cb2c414e8bdc3464f9af6aa1ab4cb44a7a1c
gKStGt <galipk@arcor.de>
2020-11-03 10:40:08 +0000

    05_6c

--- 05_SQL_SELECT_stocks/05_6b_que_count.sql
+++ 05_SQL_SELECT_stocks/05_6b_que_count.sql
@@ -32,16 +32,48 @@ ORDER BY sector ASC;
 */
 
 -- Welche VERSCHIDENEN Branchen gibt es in den jeweiligen Industriesektoren ??
-
+/*
 SELECT 
     sector AS "Industriesektoren",
-    COUNT(DISTINCT Industry) AS "Branchenanzahl"
+    COUNT(DISTINCT Industry)     "Branchenanzahl"
 FROM stocks.ccc_list
-WHERE sector LIKE "%ials"
+#WHERE sector LIKE "%ials" --> Filter nicht agg. Felder 
 GROUP BY sector
-     
+#HAVING Branchenanzahl > 10 # --> Filter agg. Felder 
 #ORDER BY COUNT(DISTINCT Industry) DESC;
 ORDER BY Branchenanzahl DESC;
+*/
+
+-- Wiviele Firmen gibt es in den Branchen ??
+/*
+SELECT 
+    industry AS "Branchen",
+    COUNT(industry) "Firmenanzahl"
+FROM stocks.ccc_list
+GROUP BY industry
+HAVING Firmenanzahl >= 10
+ORDER BY Firmenanzahl DESC;
+*/
+
+-- Wieviele Untenhmen zahlen 12/4/2/1 ??
+SELECT 
+    payouts "Auszahlungen p.a.",
+    COUNT(payouts) "Anzahl"
+FROM stocks.ccc_list
+GROUP BY payouts
+ORDER BY payouts DESC;
+
+
+-- Welche Untenhmen zahlen 12/4/2/1 ??
+
+SELECT 
+    ticker "SYM",
+    c_name "Unternhmen",
+    sector "Industriesektor"
+FROM stocks.ccc_list
+WHERE payouts = 12
+ORDER BY sector ASC;
+
 
 
 
