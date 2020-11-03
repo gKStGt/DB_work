703ca4b3fae35bcbdb02efcb971c3bbb301338f7
gKStGt <galipk@arcor.de>
2020-11-03 13:27:45 +0000

    05_6f Auszahlungsrhytmus

--- 05_SQL_SELECT_stocks/05_6d_que_avg.sql
+++ 05_SQL_SELECT_stocks/05_6d_que_avg.sql
@@ -12,7 +12,8 @@ WHERE  industry = "Banks"
 LIMIT 20;
 */
 
--- KOMBINATION Preis/ Dividenden 
+-- KOMBINATION Preis/ Dividenden
+/*
 SELECT
     DISTINCT sector AS "Industriesektor",
     ROUND (AVG(price),2) AS "Durchschnittspreis eienr Aktie",
@@ -22,5 +23,16 @@ GROUP BY sector
 #ORDER BY AVG(price) DESC
 ORDER BY AVG(dividend) DESC
 ;
+*/
 
+-- Auszahlungsrhytmus
+/**/
+SELECT 
+	payouts AS "Rhythm",
+    REPLACE(REPLACE(REPLACE(REPLACE(payouts,"12","monthly"),"4","quarterly"),"2","half-yearly"),"1","yearly") 
+    AS "Rhythm by Name", 
+	count(payouts) AS "No. of Companies"
+FROM ccc_list 
+GROUP BY payouts
+ORDER BY count(payouts) DESC; 
 
